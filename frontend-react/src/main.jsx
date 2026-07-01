9ba571eed5b47ce858ff5276e9dede54789c0e1c
Juan David Florez <juanda24florezvizcaino@gmail.com>
2026-06-30 23:17:22 -0500

    feat: bring over productos.json and existing stylesheets

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './styles/variables.css'
+import './styles/tienda1.css'
+import './styles/catalogo.css'
+import './styles/producto-detalle.css'
+import './styles/inicioRegistro.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
